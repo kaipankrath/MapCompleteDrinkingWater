d7d6468037342c4eb88d391f0597bc7d2733a05a
rezaulhasan <roconmachine@gmail.com>
2022-07-12 12:53:58 +0200

    chart

--- test.ts
+++ test.ts
@@ -1,51 +1,74 @@
-import {UIEventSource} from "./Logic/UIEventSource";
-import TagRenderingQuestion from "./UI/Popup/TagRenderingQuestion";
-import TagRenderingConfig from "./Models/ThemeConfig/TagRenderingConfig";
-import {RadioButton} from "./UI/Input/RadioButton";
-import {FixedInputElement} from "./UI/Input/FixedInputElement";
-import {VariableUiElement} from "./UI/Base/VariableUIElement";
-import ValidatedTextField from "./UI/Input/ValidatedTextField";
-import VariableInputElement from "./UI/Input/VariableInputElement";
+import { AllKnownLayouts } from "./Customizations/AllKnownLayouts";
+import * as data from "./test_data.json";
+// import {Chart} from "chart.js/auto";
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
-const config = new TagRenderingConfig({
-    question: "What is the name?",
-    render: "The name is {name}",
-    freeform: {
-        key: 'name',
-        inline:true
-    },
-    mappings:[
-        {
-            if:"noname=yes",
-            then: "This feature has no name"
+
+
+var foodLayer = AllKnownLayouts.AllPublicLayers().find(x => x.id == "food");
+// console.log(foodLayer);
+// var wheelchairTagrendering  = foodLayer.tagRenderings.find(x=>x.id == "")
+
+for (const tagrendering of foodLayer.tagRenderings) {
+    var known = data.features.filter(x =>  tagrendering.IsKnown(x.properties))
+    var statistics  = [];
+    for (const item of known) {
+        if(tagrendering.mappings == undefined)
+            continue;
+        for (const mapping of tagrendering.mappings) {
+
+            if(mapping.if.matchesProperties(item.properties))
+            {
+                var key = mapping.then.textFor("en");
+                if(statistics[key] == undefined) statistics[key] = [];
+                if(statistics[key][item.properties[key]] == undefined) statistics[key][item.properties[key]] = 0;
+                statistics[key][item.properties[key]] ++;
+            }
         }
-    ]
-})
-
-const tags = new UIEventSource<any>({
-    name: "current feature name"
-})
-
-/*new TagRenderingQuestion(
-    tags, config, undefined).AttachTo("maindiv")*/
-const options = new UIEventSource<string[]>([])
-const rb =
-    new VariableInputElement(
-        options.map(options  => {
-            console.trace("Construction an input element for", options)
-           return new RadioButton(
-                [
-                    ...options.map(o => new FixedInputElement(o,o)),
-                    new FixedInputElement<string>("abc", "abc"),
-                    ValidatedTextField.ForType().ConstructInputElement()
-                ])
-        }
-        
-    )
+    }
+    console.log(statistics);
+   
     
-)
-rb.AttachTo("maindiv")
-rb.GetValue().addCallbackAndRun(v => console.log("Current value is",v))
-new VariableUiElement(rb.GetValue()).AttachTo("extradiv")
+}
 
-window.setTimeout(() => {options.setData(["xyz","foo","bar"])},10000)
+
+
+const ctx:any = document.getElementById('myChart');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
+
